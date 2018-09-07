95584f3872f92ce6f0b364feef8f7ae536973207
Yeison Gomez <yeigomez97@hotmail.com>
2018-09-06 22:41:15 -0500

    feat(geoserver): connect cesium to geoserver

--- src/app/shared/directives/cesium.directive.ts
+++ src/app/shared/directives/cesium.directive.ts
@@ -19,10 +19,29 @@ export class CesiumDirective implements OnInit {
 			navigationHelpButton: false,
 			navigationInstructionsInitiallyVisible: false,
 			animation: false,
-			timeline: false
+			timeline: false,
+			/*imageryProvider : new Cesium.ArcGisMapServerImageryProvider({
+		        url : '//services.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer'
+		    })*/
 		});
+
+		var imageryLayers = this.viewer.imageryLayers; 
+		imageryLayers.addImageryProvider(new Cesium.WebMapServiceImageryProvider(
+			{ 
+				url : 'http://016e57be.ngrok.io/geoserver/prueba/wms', 
+				layers : 'prueba:T18NVJ_20180901T152629_TCI1', 
+			}));
+
 		
 		this.cesiumService.init(this.viewer);
+		/*var widget = new Cesium.CesiumWidget(this.el.nativeElement);
+		var url='http://016e57be.ngrok.io/geoserver/wms'; //Geoserver URL
+		var layers = widget.scene.globe.imageryLayers;
+		    layers.removeAll();
+		    layers.addImageryProvider(new Cesium.WebMapServiceImageryProvider({
+		        url : url,        
+		        layers: 'prueba'// Here just give layer name
+		    }));*/
 		//this.cesiumService.paintPoints();
 		//this.cesiumService.model3D();
 
